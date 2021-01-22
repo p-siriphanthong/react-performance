ad21e5181c2b47f41abf3fbc6eeec126db9dc9c3
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-01-23 04:33:09 +0700

    do exercise 1

--- src/exercise/01.extra-1.js
+++ src/exercise/01.extra-1.js
@@ -1,3 +1,53 @@
 // Code splitting
 // 💯 eager loading
 // http://localhost:3000/isolated/exercise/01.extra-1.js
+
+import * as React from 'react'
+
+function lazyWithPreload(factory) {
+  const Component = React.lazy(factory)
+  Component.preload = factory
+  return Component
+}
+
+const Globe = lazyWithPreload(() => import('../globe'))
+
+function App() {
+  const [showGlobe, setShowGlobe] = React.useState(false)
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: '100%',
+        padding: '2rem',
+      }}
+    >
+      <label
+        style={{marginBottom: '1rem'}}
+        onMouseOver={() => Globe.preload()}
+        onFocus={() => Globe.preload()}
+      >
+        <input
+          type="checkbox"
+          checked={showGlobe}
+          onChange={e => setShowGlobe(e.target.checked)}
+        />
+        {' show globe'}
+      </label>
+      <div style={{width: 400, height: 400}}>
+        <React.Suspense fallback={<div>loading globe...</div>}>
+          {showGlobe ? <Globe /> : null}
+        </React.Suspense>
+      </div>
+    </div>
+  )
+}
+// 🦉 Note that if you're not on the isolated page, then you'll notice that this
+// app actually already has a React.Suspense component higher up in the tree
+// where this component is rendered, so you *could* just rely on that one.
+
+export default App
